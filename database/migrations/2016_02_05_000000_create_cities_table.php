4f4e27c77e5f91dbf3dc5e0c5ca5f1e7b5ba3206
Magno Biét <magno.biet@gmail.com>
2016-02-05 22:43:56 -0200

    Add migrations

---
+++ database/migrations/2016_02_05_000000_create_cities_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCitiesTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('cities', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->integer('state_id')->unsigned();
+            $table->foreign('state_id')->references('id')->on('states');
+            $table->string('name', 64);
+            $table->timestamps();
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::drop('cities');
+
+    }
+
+}
---
+++ database/migrations/2016_02_05_000000_create_states_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateStatesTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('states', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('name', 64)->unique();
+            $table->string('abbr', 2)->unique();
+            $table->timestamps();
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::drop('states');
+
+    }
+
+}
