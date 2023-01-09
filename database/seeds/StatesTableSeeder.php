2f463b8367247d6c3078d0867d06b5989350c4c5
Magno Fernando Brixner Biét <magno.biet@gmail.com>
2023-01-09 08:34:37 -0300

    fix: add seeds namespace

--- database/seeds/StatesTableSeeder.php
+++ database/seeds/StatesTableSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class StatesTableSeeder extends Seeder
 {
